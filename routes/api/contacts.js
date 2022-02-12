5c810e185c19ba12b0fe64cde50499d32dc3366e
Elena Semeshyna <81982732+LenaXmi@users.noreply.github.com>
2022-02-12 21:40:33 +0100

    get/post routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,48 @@
 const express = require('express')
+const contacts = require('../../models/contacts.json')
+const {nanoid} = require('nanoid')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({
+    status: 'success',
+    code: 200,
+    data:{result:contacts}
+    
+  })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const result = contacts.find(contact => contact.id === contactId)
+  if (!result) {
+    res.status(404).json({
+      status: 'error',
+      code: 404,
+      message:`Contact with id=${contactId} not found`
+    })
+  }
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result
+    }
+    })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const newContact = { ...req.body, id: nanoid(5) }
+  contacts.push(newContact)
+  res.status(201).json({
+    status: 'success',
+    code: 201,
+    data: {
+      result:newContact
+    }
+})
+  
 })
 
 router.delete('/:contactId', async (req, res, next) => {
